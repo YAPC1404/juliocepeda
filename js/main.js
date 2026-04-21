be5f45b47c253410a2a8a760aaf761fa497774e0
YAPC1404 <YAPC1404@hotmail.com>
2026-04-21 12:07:29 -0600

    avance index y cheeckout con cambios de diego incluidos

--- js/main.js
+++ js/main.js
@@ -9,3 +9,28 @@ btnTheme.addEventListener('click', () => {
     const isDark = document.body.classList.contains('dark-theme');
     localStorage.setItem('theme', isDark ? 'dark' : 'light');
 });
+
+const slider = document.querySelector(".slider");
+const slides = document.querySelectorAll(".slide");
+
+let index = 0;
+
+function updateSlide() {
+  slider.style.transform = `translateX(-${index * 100}%)`;
+}
+
+document.querySelector(".next").onclick = () => {
+  index = (index + 1) % slides.length;
+  updateSlide();
+};
+
+document.querySelector(".prev").onclick = () => {
+  index = (index - 1 + slides.length) % slides.length;
+  updateSlide();
+};
+
+
+setInterval(() => {
+  index = (index + 1) % slides.length;
+  updateSlide();
+}, 4000);
